ef9a33c7959508a9b7c752d1669974a009122658
Mirko Brombin <send@mirko.pm>
2022-12-03 14:53:00 +0100

    upload local work

---
+++ src/main.py
@@ -0,0 +1,102 @@
+# main.py
+#
+# Copyright 2022 Mirko Brombin <send@mirko.pm>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import sys
+import gi
+
+gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
+
+from gi.repository import Gtk, Gio, GLib, Adw
+from .window import AdwdialogWindow
+
+
+class AdwdialogApplication(Adw.Application):
+    """The main application singleton class."""
+
+    title, description, icon, dtype = None, None, None, None
+
+    def __init__(self):
+        super().__init__(application_id='org.vanillaos.AdwDialog',
+                         flags=Gio.ApplicationFlags.HANDLES_COMMAND_LINE)
+        self.create_action('quit', self.quit, ['<primary>q'])
+        self.__register_arguments()
+
+    def __register_arguments(self):
+        """Register the command line arguments."""
+        self.add_main_option('title', ord('t'), GLib.OptionFlags.NONE,
+                             GLib.OptionArg.STRING, 'The dialog title', None)
+        self.add_main_option('description', ord('d'), GLib.OptionFlags.NONE,
+                             GLib.OptionArg.STRING, 'The dialog description', None)
+        self.add_main_option('icon', ord('i'), GLib.OptionFlags.NONE,
+                             GLib.OptionArg.STRING, 'The dialog icon', None)
+        self.add_main_option('type', ord('y'), GLib.OptionFlags.NONE,
+                             GLib.OptionArg.STRING, 'The dialog type', None)
+
+    def do_command_line(self, command_line):
+        """Handle command line arguments."""
+        options = command_line.get_options_dict()
+        if options.contains('title'):
+            self.title = options.lookup_value('title').get_string()
+        if options.contains('description'):
+            self.description = options.lookup_value('description').get_string()
+        if options.contains('icon'):
+            self.icon = options.lookup_value('icon').get_string()
+        if options.contains('type'):
+            self.dtype = options.lookup_value('type').get_string()
+        self.activate()
+        return 0
+
+    def do_activate(self):
+        """Called when the application is activated.
+
+        We raise the application's main window, creating it if
+        necessary.
+        """
+        if None in (self.title, self.description, self.dtype):
+            print('Missing essential arguments: --title --description --type')
+            sys.exit(1)
+
+        win = self.props.active_window
+        if not win:
+            win = AdwdialogWindow(application=self, title=self.title,
+                                  description=self.description, icon=self.icon,
+                                  dtype=self.dtype)
+        win.present()
+
+    def create_action(self, name, callback, shortcuts=None):
+        """Add an application action.
+
+        Args:
+            name: the name of the action
+            callback: the function to be called when the action is
+              activated
+            shortcuts: an optional list of accelerators
+        """
+        action = Gio.SimpleAction.new(name, None)
+        action.connect("activate", callback)
+        self.add_action(action)
+        if shortcuts:
+            self.set_accels_for_action(f"app.{name}", shortcuts)
+
+
+def main(version):
+    """The application's entry point."""
+    app = AdwdialogApplication()
+    return app.run(sys.argv)
---
+++ src/window.py
@@ -0,0 +1,73 @@
+# window.py
+#
+# Copyright 2022 Mirko Brombin <send@mirko.pm>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import sys
+from gi.repository import Adw
+from gi.repository import Gtk
+
+@Gtk.Template(resource_path='/org/vanillaos/AdwDialog/window.ui')
+class AdwdialogWindow(Adw.ApplicationWindow):
+    __gtype_name__ = 'AdwdialogWindow'
+
+    status = Gtk.Template.Child()
+    box_btns = Gtk.Template.Child()
+
+    def __init__(self, title, description, icon, dtype, **kwargs):
+        super().__init__(**kwargs)
+
+        self.set_title(title)
+        self.status.set_title(title)
+        self.status.set_description(description)
+
+        if icon:
+            self.status.set_icon_name(icon)
+        elif dtype == "info":
+            self.status.set_icon_name("dialog-information-symbolic")
+        elif dtype == "warning":
+            self.status.set_icon_name("dialog-warning-symbolic")
+        elif dtype == "error":
+            self.status.set_icon_name("dialog-error-symbolic")
+        elif dtype == "question":
+            self.status.set_icon_name("dialog-question-symbolic")
+
+        self.__build_buttons(dtype)
+
+    def __build_buttons(self, dtype):
+        if dtype == "info":
+            self.__add_button("Ok", 0, True)
+        elif dtype == "warning":
+            self.__add_button("Ok", 0, True)
+        elif dtype == "error":
+            self.__add_button("Ok", 0, True)
+        elif dtype == "question":
+            self.__add_button("Yes", 0, True)
+            self.__add_button("No", 1)
+
+    def __add_button(self, label, action, suggested=False):
+        btn = Gtk.Button(label=label)
+        btn.connect("clicked", self.__on_button_clicked, action)
+        btn.add_css_class("pill")
+
+        if suggested:
+            btn.add_css_class("suggested-action")
+
+        self.box_btns.append(btn)
+
+    def __on_button_clicked(self, widget, action):
+        sys.exit(action)
